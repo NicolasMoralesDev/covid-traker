3aa01b1906d6ea491770b334cd7fd965facb5a67
Nicolas Morales <nicolasmoralesj03@gmail.com>
2024-06-28 19:12:18 -0300

    se terina el ruteo de la app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import DataBar from './Components/common/aside/DataBar'
+import TopTen from './Components/common/aside/TopTen'
+import SideBar from './Components/common/sidebar/SideBar'
+//import getData from "./services/getData";
+import useApi from './services/useApi';
+import { API_WORLD_TOTAL, API_COUNTRY_TOTAL } from './config/urls';
+import './App.css'
+
+function App()  {
+  (
+  <>
+    </>
+  )
+}
+export default App
+
+
+
+
+
+
+
